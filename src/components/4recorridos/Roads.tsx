3e1bbde64beeb4b61e26ca75bd976b5e3ced679a
lopezander <ander.jeffrey@gmail.com>
2025-11-28 18:38:37 -0500

    update roads routes

--- src/components/4recorridos/Roads.tsx
+++ src/components/4recorridos/Roads.tsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import CognitiveRouter from "./cognitive/CognitiveRouter";
-import HomePage from "./homepage/HomePage";
+
 
 export default function Roads() {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
             <Route path="/cognitivo/*" element={<CognitiveRouter />} />
         </Routes>
     );
