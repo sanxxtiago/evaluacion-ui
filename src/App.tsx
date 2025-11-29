f13c5fb7b2a5e86d5130c65568c184b84fe3fb45
lopezander <ander.jeffrey@gmail.com>
2025-11-28 20:40:05 -0500

    update routes roads and heuristic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MainLayout } from "./layout/MainLayout"
 import { Home } from "./components/homepage/Home";
-import { Heuristics } from "./components/1heuristica/Heuristics";
 import { Users } from "./components/2pruebas/UsersPage"
 import { FittsModelPage } from "./components/3modelos/FittsModelPage";
-import { Roads } from "./components/4recorridos/Roads";
 import { Subjective } from "./components/5subjetiva/Subjective";
+import HeuristicsModule from "./components/1heuristica/HeuristicModule";
+import Roads from "./components/4recorridos/Roads";
 
 const router = createBrowserRouter([
   {
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { index: true, element: <Home /> },
-      { path: "heuristics", element: <Heuristics /> },
+      { path: "heuristics", element: <HeuristicsModule /> },
       { path: "users", element: <Users /> },
       { path: "models", element: <FittsModelPage /> },
       { path: "roads", element: <Roads /> },
